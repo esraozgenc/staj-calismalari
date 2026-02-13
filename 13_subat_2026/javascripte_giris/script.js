10a754cb52c2e28e6979d34266c63674f1f46c50
esraozgenc <esraozgenc572@gmail.com>
2026-02-14 02:13:55 +0300

    Javascript ile çıktı verme yöntemleri eklendi.

--- 13_subat_2026/javascripte_giris/script.js
+++ 13_subat_2026/javascripte_giris/script.js
@@ -1,1 +1,28 @@
-console.log("Hello World")
+//Çıktı verme yöntemleri
+
+//1) document.writeln
+document.writeln("Esra Özgenç <br>"); //<br> etiketi alt satıra geçmesini sağlıyor.
+document.write(42 + "<br>"); // + sembolü de ilk girilen değer int olduğu için.
+document.writeln(5.5);
+document.writeln(true);
+
+//2) console.log(): Web sitesinde inceleye basınca konsol kısmında bu loglar görünür.
+console.log("Hello World");
+console.log("Esra Özgenç");
+console.log(27.5);
+console.log(false);
+console.log({name:"Esra" , surname:"Özgenç"});
+
+//console.clear()
+
+let a = 5;
+let b = 10;
+
+console.log(a+b);
+console.log("iki sayının toplamı: ",a+b);
+console.log("iki sayının toplamı: " + (a+b));
+
+//3) alert
+
+alert("iki sayının toplamı: "+ (a+b));
+alert("Lütfen f5 tuşuna basmayınız!");
