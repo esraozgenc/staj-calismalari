87c1f0dba454a4579418c07f47dd0ab9d0e827f0
esraozgenc <esraozgenc572@gmail.com>
2026-02-15 01:40:36 +0300

    Javascript ile degisken tanımlama yöntemleri eklendi.

---
+++ 14_subat_2026/script.js
@@ -0,0 +1,20 @@
+var full_name = "Esra Ozgenc"
+console.log(full_name)
+
+console.log(hata) //degisken daha sonra verildigi icin "var" turunde undefined yazar.
+var hata = 789;
+
+//console.log(hata2) //let turunde konsol ekranına hatayı yazdırır.
+//let hata2 = 567;
+
+let tel_no = 12345
+console.log(tel_no)
+
+tel_no = 54321 // let turunde degisken degistirilebilir
+console.log(tel_no)
+
+tel_no += 0 + " Telefon numarası"; //birlestirme ve degiskenin icine ekleme islemi
+console.log(tel_no)
+
+const tc_kimlik = "123456789" //const tanımlarken bir deger atamak zorundayız yoksa hata verir. Ayrıca const ile atanan degerler degistirilemez.
+console.log(tc_kimlik)
