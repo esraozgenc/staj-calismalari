a59d5a92cdeab08874249bbc751eedd5d6839035
esraozgenc <esraozgenc572@gmail.com>
2026-02-16 23:54:58 +0300

    Javascript ile aritmetik ve atama operatörleri eklendi.

---
+++ 16_subat_2026/js_veri_tipleri/script.js
@@ -0,0 +1,29 @@
+let a = 15;
+a++; //artırma operatörüdür sayıyı 1 arttırır
+a--; //azaltma operatörüdür sayıyı 1 azaltır
+console.log(a)
+
+console.log(4**2) //bir sayının üssünü almak için kullanılır
+
+// = atama operatörüdür
+// == eşittir operatörüdür. Veri tipinin string veya number olduğuna bakmaz içeriğine bakar.
+// === hem içindeki değerlerin hem de değerlerinin eşit olup olmadığına bakar
+
+let sayi = 4
+sayi -= 1 //çıkarma yapmak için -= operatörü kullanılır.
+console.log("Çıkarma işlemi 4-1=" + sayi)
+
+sayi *= 2
+console.log("Çarpma işlemi 3*2=" + sayi)
+
+sayi /= 3
+console.log("Bölme işlemi 6/3=" + sayi)
+
+sayi = 13
+sayi %= 2
+console.log("Modu alınan sayının modu 13%2=" + sayi)
+
+sayi=2
+sayi **=5
+console.log("Üssü alınan sayı 2**5=" + sayi)
+
