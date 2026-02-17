13f7aee3bdf03c9e59a156f913de5b052d282c2b
esraozgenc <esraozgenc572@gmail.com>
2026-02-17 14:43:22 +0300

    Javascript ile mantıksal operatörler eklendi.

---
+++ 17_subat_2026/js_operatorler_2/script.js
@@ -0,0 +1,12 @@
+let yas = 20;
+let para =3500;
+let saglikliMi = false;
+let hakSayisi = 3;
+
+console.log(yas>18 && para>3000 && saglikliMi==true) //&& ve operatörü
+
+console.log(5>3 || 2<3); // || veya operatörü
+
+console.log(!(5>3)) // değil operatörü !
+
+console.log((para>3000 || hakSayisi>1) || (yas>18 && saglikliMi))
