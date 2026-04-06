91d1ea2917ba7b5cfb066f921e84381ebb3d12ca
esraozgenc <esraozgenc572@gmail.com>
2026-04-06 20:00:37 +0300

    Pixel e-ticaret sitesinde footer component'in css kodları eklendi.

--- pixel_store/components/footer.js
+++ pixel_store/components/footer.js
@@ -2,22 +2,23 @@ function footerHTML(){
     return`
     <footer>
         <div class="footer-brand">
-            <h3>PIXEL</h3>
+            <h4>PIXEL</h4>
             <p>Detay arttıkça PIXEL büyür.</p>
         </div>
         <div class="footer-links">
-            <h3>Hızlı Linkler</h3>
-            <a href="/pixel_store/index.html">Anasayfa</a>
-            <a href="/pixel_store/pages/cart.html">Sepet</a>
-            <a href="/pixel_store/pages/profile.html">Profil</a>
-            <a href="/pixel_store/pages/category-computer.html">Bilgisayar</a>
-            <a href="/pixel_store/pages/category-tablet.html">Tablet</a>
-            <a href="/pixel_store/pages/category-phone.html">Telefon</a>
-            <a href="/pixel_store/pages/category-tv.html">Televizyon</a>
+            <h4>Hızlı Linkler</h4>
+            <div class="footer-links-list">
+                <a href="/pixel_store/index.html">Anasayfa</a>
+                <a href="/pixel_store/pages/cart.html">Sepet</a>
+                <a href="/pixel_store/pages/profile.html">Profil</a>
+                <a href="/pixel_store/pages/category-computer.html">Bilgisayar</a>
+                <a href="/pixel_store/pages/category-tablet.html">Tablet</a>
+                <a href="/pixel_store/pages/category-phone.html">Telefon</a>
+                <a href="/pixel_store/pages/category-tv.html">Televizyon</a>
+            </div>
         </div>
         <div class="footer-contact">
-            <h3>İletişim</h3>
-            <p>Bize ulaşın</p>
+            <h4>İletişim</h4>
             <a href="mailto:destek@pixelstore.com">destek@pixelstore.com</a>
         </div>
         <div class="footer-copyright">
