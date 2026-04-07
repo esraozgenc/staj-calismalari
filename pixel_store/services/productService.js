0cea27fa33eb9187e45c04616eed34ee12986ca5
esraozgenc <esraozgenc572@gmail.com>
2026-04-07 21:44:43 +0300

    Pixel e-ticaret sitesinde product service kodları eklendi.

---
+++ pixel_store/services/productService.js
@@ -0,0 +1,23 @@
+//Tüm ürünleri getir
+async function getAllProducts(){
+    try {
+        const response = await fetch("../data/products.json")
+        const data = await response.json()
+        return data
+    } catch (err) {
+        console.log(err)
+        return []
+    }
+}
+
+//Bir ürünü ID ile getir
+async function getProductById(productId){
+    const product = await getAllProducts()
+    return product.find(item => item.id === productId) || null
+}
+
+//Kategorideki ürünleri getir
+async function getProductsByCategory(categoryId) {
+    const products = await getAllProducts()
+    return products.filter(item => item.categoryId === categoryId)
+}
